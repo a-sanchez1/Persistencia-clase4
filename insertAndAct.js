683f2c2c0f27ff04cb4a1667dda0800b80a6c048
alberto leandro sanchez <62775641+a-sanchez1@users.noreply.github.com>
2020-09-06 18:38:05 -0300

    Add files via upload

---
+++ insertAndAct.js
@@ -0,0 +1,63 @@
+const {Sequelize} = require('sequelize')
+
+const sequelize = new Sequelize('clase4','root','',{
+    host: 'localhost',
+    dialect: 'mariadb'
+})
+
+sequelize.authenticate().then(()=>{
+    console.log('Connection has been established successfully.')
+  }).catch(error => {
+    console.error('Unable to connect to the database:', error);
+  });
+
+
+//aqui armamos el modelo a armar
+
+class Pelicula extends Sequelize.Model {}
+Pelicula.init({
+   nombre: Sequelize.STRING,
+   director: Sequelize.STRING,
+   anio:Sequelize.INTEGER
+}, { sequelize, modelName: 'pelicula' });
+
+
+//aqui instanciamos
+
+Pelicula.sync().then(() =>
+    Pelicula.create({
+        nombre: "Tiburon",
+        director:"Steven Spilberg",
+        anio: 1975
+    })
+)
+
+Pelicula.sync().then(() =>
+    Pelicula.create({
+        nombre: "ET",
+        director:"Steven Spilberg",
+        anio: 1985
+    })
+)
+
+Pelicula.sync().then(() =>
+    Pelicula.create({
+        nombre: "Lista de schindler",
+        director:"Steven Spilberg",
+        anio: 1993
+    })
+)
+
+Pelicula.sync().then(() =>
+    Pelicula.create({
+        nombre: "Avengers",
+        director:"Jhon Favrau",
+        anio: 2015
+    })
+)
+
+Nombre.update({ Nombre: "Vengadores" }, {
+  where: {
+    director: 'Jhon Favrau'
+  }
+})
